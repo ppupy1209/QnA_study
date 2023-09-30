76d26d50d7b5d519941f66ea9de281eee6406991
yeonwoo <ppupy1209@naver.com>
2023-09-30 19:42:11 +0900

    findByName 추가

--- src/main/java/toyproject/qna/module/member/repository/MemberRepository.java
+++ src/main/java/toyproject/qna/module/member/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package toyproject.qna.module.member.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import toyproject.qna.module.member.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByName(String name);
 }
