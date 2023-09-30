10d09bcff6fecc0780f302c1753add19f63f1e4b
yeonwoo <ppupy1209@naver.com>
2023-09-30 22:37:47 +0900

    GlobalExceptionAdvice,ErrorResponse 추가

---
+++ src/main/java/toyproject/qna/global/exception/ErrorResponse.java
@@ -0,0 +1,23 @@
+package toyproject.qna.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.validation.FieldError;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private List<FieldError> fieldErrors;
+
+
+    @AllArgsConstructor
+    @Getter
+    public static class FieldError {
+        private String field;
+        private Object rejectedValue;
+        private String reason;
+    }
+}
