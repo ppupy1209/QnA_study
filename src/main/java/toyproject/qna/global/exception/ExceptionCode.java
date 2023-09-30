140eaf9ba05820ff75f67ab887cebfd2eee02a0a
yeonwoo <ppupy1209@naver.com>
2023-09-30 22:43:06 +0900

    BusinessLogicException,ExceptionCode 추가

---
+++ src/main/java/toyproject/qna/global/exception/ExceptionCode.java
@@ -0,0 +1,18 @@
+package toyproject.qna.global.exception;
+
+import lombok.Getter;
+
+public enum ExceptionCode {
+
+    MEMBER_NOT_FOUND(404,"Member Not Found");
+
+    @Getter
+    private int status;
+    @Getter
+    private String message;
+
+    ExceptionCode(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
