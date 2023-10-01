e181be876a88a12a92ed774897c3ba3652dd4543
yeonwoo <ppupy1209@naver.com>
2023-10-01 16:54:29 +0900

    SingleResponseDto 추가

---
+++ src/main/java/toyproject/qna/global/dto/SingleResponseDto.java
@@ -0,0 +1,11 @@
+package toyproject.qna.global.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SingleResponseDto<T> {
+
+    private T data;
+}
