93bd19a0a9112faf249c6e4af170faa646a4f915
yeonwoo <ppupy1209@naver.com>
2023-10-02 16:04:11 +0900

    findByName 추가

--- src/main/java/toyproject/qna/module/tag/repository/TagRepository.java
+++ src/main/java/toyproject/qna/module/tag/repository/TagRepository.java
@@ -3,5 +3,9 @@ package toyproject.qna.module.tag.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import toyproject.qna.module.tag.entity.Tag;
 
+import java.util.Optional;
+
 public interface TagRepository extends JpaRepository<Tag, Long> {
+
+    Optional<Tag> findByName(String name);
 }
