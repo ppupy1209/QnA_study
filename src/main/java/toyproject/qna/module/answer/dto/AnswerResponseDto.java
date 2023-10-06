f71da8334b557571690bf461d4cd7a2c9aabd13e
yeonwoo <ppupy1209@naver.com>
2023-10-06 22:07:54 +0900

    List 생성자 추가

--- src/main/java/toyproject/qna/module/answer/dto/AnswerResponseDto.java
+++ src/main/java/toyproject/qna/module/answer/dto/AnswerResponseDto.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import toyproject.qna.module.answer.entity.Answer;
 import toyproject.qna.module.member.entity.Member;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Getter
 @Builder
 public class AnswerResponseDto {
@@ -19,5 +22,11 @@ public class AnswerResponseDto {
                 .memberName(answer.getMember().getName())
                 .content(answer.getContent())
                 .build();
+    }
+
+    public static List<AnswerResponseDto> of(List<Answer> answers) {
+        return answers.stream()
+                .map(answer -> AnswerResponseDto.of(answer))
+                .collect(Collectors.toList());
     }
 }
