4890993a6211e0e64d651773386f3615456b099e
yeonwoo <ppupy1209@naver.com>
2023-10-23 18:23:55 +0900

    Repository

--- src/main/java/toyproject/qna/module/item/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package toyproject.qna.module.item.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import toyproject.qna.module.item.entity.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
--- src/main/java/toyproject/qna/module/item/repository/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package toyproject.qna.module.item.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import toyproject.qna.module.item.entity.Movie;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-}
