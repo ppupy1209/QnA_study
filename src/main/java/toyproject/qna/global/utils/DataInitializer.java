b5b90388639f22b43fdf7e6f56cd9c84d634e00b
yeonwoo <ppupy1209@naver.com>
2023-10-23 18:23:17 +0900

    초기 데이터 추가

--- src/main/java/toyproject/qna/global/utils/DataInitializer.java
+++ src/main/java/toyproject/qna/global/utils/DataInitializer.java
@@ -2,11 +2,10 @@ package toyproject.qna.global.utils;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import toyproject.qna.module.answer.repository.AnswerRepository;
+import toyproject.qna.module.item.entity.Item;
+import toyproject.qna.module.item.repository.ItemRepository;
 import toyproject.qna.module.member.entity.Member;
 import toyproject.qna.module.member.repository.MemberRepository;
-import toyproject.qna.module.question.repository.QuestionRepository;
-import toyproject.qna.module.tag.repository.TagRepository;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -16,6 +15,7 @@ import java.util.List;
 public class DataInitializer {
 
     private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
 
     @PostConstruct
     public void init() {
@@ -26,12 +26,26 @@ public class DataInitializer {
         Member member5 = createMember("5번", 50);
 
         memberRepository.saveAll(List.of(member1,member2,member3,member4,member5));
+
+        Item item1 = createItem("책",100,5000);
+        Item item2 = createItem("영화",50,7000);
+        Item item3 = createItem("음악",70,3000);
+
+        itemRepository.saveAll(List.of(item1,item2,item3));
     }
 
     private Member createMember(String name, int age) {
         return Member.builder()
                 .name(name)
                 .age(age)
+                .build();
+    }
+
+    private Item createItem(String name, int stockQuantity, int price) {
+        return Item.builder()
+                .name(name)
+                .stockQuantity(stockQuantity)
+                .price(price)
                 .build();
     }
 }
