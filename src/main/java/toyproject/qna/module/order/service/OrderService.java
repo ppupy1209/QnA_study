8e788a36e1006a5dd49b7951d8752aa5a786ac79
yeonwoo <ppupy1209@naver.com>
2023-10-24 16:15:21 +0900

    Service

--- src/main/java/toyproject/qna/module/order/service/OrderService.java
+++ src/main/java/toyproject/qna/module/order/service/OrderService.java
@@ -3,6 +3,8 @@ package toyproject.qna.module.order.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import toyproject.qna.global.exception.BusinessLogicException;
+import toyproject.qna.global.exception.ExceptionCode;
 import toyproject.qna.module.address.Address;
 import toyproject.qna.module.delivery.entity.Delivery;
 import toyproject.qna.module.item.entity.Item;
@@ -10,11 +12,14 @@ import toyproject.qna.module.item.repository.ItemRepository;
 import toyproject.qna.module.item.servcie.ItemService;
 import toyproject.qna.module.member.entity.Member;
 import toyproject.qna.module.member.service.MemberService;
+import toyproject.qna.module.order.dto.OrderItemDto;
 import toyproject.qna.module.order.dto.OrderPostDto;
 import toyproject.qna.module.order.entity.Order;
 import toyproject.qna.module.order.entity.OrderItem;
 import toyproject.qna.module.order.repository.OrderRepository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 @Transactional
 @RequiredArgsConstructor
@@ -25,20 +30,38 @@ public class OrderService {
     private final ItemService itemService;
     public Long createOrder(OrderPostDto orderPostDto) {
         Member member = memberService.findMember(orderPostDto.getMemberId());
-        Item item = itemService.findItem(orderPostDto.getItemId());
-
         Address address = Address.createAddress(orderPostDto.getCity(), orderPostDto.getStreet(), orderPostDto.getZipcode());
-
         Delivery delivery = Delivery.createDelivery(address);
 
-        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(),orderPostDto.getQuantity());
+        List<OrderItemDto> items = orderPostDto.getItems();
+        List<OrderItem> orderItems = new ArrayList<>();
+        for (OrderItemDto item : items) {
+            Item findItem = itemService.findItem(item.getItemId());
+            OrderItem orderItem = OrderItem.createOrderItem(findItem, findItem.getPrice(),item.getQuantity());
+            orderItems.add(orderItem);
+        }
 
-        Order order = Order.createOrder(member,delivery,orderItem);
+        Order order = Order.createOrder(member,delivery,orderItems);
+        Order saveOrder = orderRepository.save(order);
 
-        Order savedOrder = orderRepository.save(order);
-
-        return savedOrder.getId();
+        return saveOrder.getId();
     }
 
-    
+    public void cancelOrder(Long orderId) {
+        Order order = findVerifiedOrder(orderId);
+
+        order.cancel();
+    }
+
+    private Order findVerifiedOrder(Long orderId) {
+        Optional<Order> optionalOrder = orderRepository.findById(orderId);
+
+        Order order = optionalOrder.orElseThrow(() -> new BusinessLogicException(ExceptionCode.ORDER_NOT_FOUND));
+
+        return order;
+    }
+
+
+
+
 }
