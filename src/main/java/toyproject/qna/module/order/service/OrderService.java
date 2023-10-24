b26273f15a0ba5815a5178d098cce073106d007a
yeonwoo <ppupy1209@naver.com>
2023-10-24 19:04:19 +0900

    OrderService

--- src/main/java/toyproject/qna/module/order/service/OrderService.java
+++ src/main/java/toyproject/qna/module/order/service/OrderService.java
@@ -1,8 +1,12 @@
 package toyproject.qna.module.order.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.support.ManagedList;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import toyproject.qna.global.dto.MultiResponseDto;
 import toyproject.qna.global.exception.BusinessLogicException;
 import toyproject.qna.global.exception.ExceptionCode;
 import toyproject.qna.module.address.Address;
@@ -14,6 +18,7 @@ import toyproject.qna.module.member.entity.Member;
 import toyproject.qna.module.member.service.MemberService;
 import toyproject.qna.module.order.dto.OrderItemDto;
 import toyproject.qna.module.order.dto.OrderPostDto;
+import toyproject.qna.module.order.dto.OrderResponseDto;
 import toyproject.qna.module.order.entity.Order;
 import toyproject.qna.module.order.entity.OrderItem;
 import toyproject.qna.module.order.repository.OrderRepository;
@@ -51,6 +56,13 @@ public class OrderService {
         Order order = findVerifiedOrder(orderId);
 
         order.cancel();
+    }
+
+    public MultiResponseDto findOrders(int page, int size) {
+        Page<Order> pagedOrders = orderRepository.findOrdersWithMemberAndDelivery(PageRequest.of(page,size));
+        List<Order> orders = pagedOrders.getContent();
+
+        return new MultiResponseDto<>(OrderResponseDto.of(orders),pagedOrders);
     }
 
     private Order findVerifiedOrder(Long orderId) {
