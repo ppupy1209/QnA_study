f7d08cb360c81933644b0bd57bdf5a0e3645070e
yeonwoo <ppupy1209@naver.com>
2023-10-28 12:29:27 +0900

    OrderPostDto

--- src/main/java/toyproject/qna/module/order/dto/OrderPostDto.java
+++ src/main/java/toyproject/qna/module/order/dto/OrderPostDto.java
@@ -1,9 +1,11 @@
 package toyproject.qna.module.order.dto;
 
+import lombok.Builder;
 import lombok.Getter;
+import toyproject.qna.module.order.entity.Order;
 
 import java.util.List;
-
+@Builder
 @Getter
 public class OrderPostDto {
     private Long memberId;
