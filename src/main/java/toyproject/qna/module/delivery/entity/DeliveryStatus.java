024dad103277548c5e1700284c8c5d4754ff322e
yeonwoo <ppupy1209@naver.com>
2023-10-28 12:06:35 +0900

    DeliveryStatus

--- src/main/java/toyproject/qna/module/delivery/entity/DeliveryStatus.java
+++ src/main/java/toyproject/qna/module/delivery/entity/DeliveryStatus.java
@@ -1,5 +1,10 @@
 package toyproject.qna.module.delivery.entity;
 
 public enum DeliveryStatus {
-    READY,CANCEL,COMP
+    // 배달 준비
+    READY,
+    // 배달 취소
+    CANCEL,
+    // 배달 완료
+    COMP
 }
