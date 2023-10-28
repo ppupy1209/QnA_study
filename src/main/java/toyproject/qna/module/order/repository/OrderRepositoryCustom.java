fd28072f88f1e86588742c7c360a492eb39905f3
yeonwoo <ppupy1209@naver.com>
2023-10-28 12:15:13 +0900

    OrderRepository

--- src/main/java/toyproject/qna/module/order/repository/OrderRepositoryCustom.java
+++ src/main/java/toyproject/qna/module/order/repository/OrderRepositoryCustom.java
@@ -7,6 +7,6 @@ import toyproject.qna.module.order.entity.Order;
 import java.util.List;
 
 public interface OrderRepositoryCustom {
-
+    // 주문 조회 | fetch join 최적화
     Page<Order> findOrdersWithMemberAndDelivery(Pageable pageable);
 }
