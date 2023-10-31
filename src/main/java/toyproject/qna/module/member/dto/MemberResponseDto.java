8cc49e81883c9fe76ea6a58c22eb9e14b2a64950
yeonwoo <ppupy1209@naver.com>
2023-10-31 16:02:16 +0900

    MemberResponseDto

--- src/main/java/toyproject/qna/module/member/dto/MemberResponseDto.java
+++ src/main/java/toyproject/qna/module/member/dto/MemberResponseDto.java
@@ -19,7 +19,6 @@ public class MemberResponseDto {
     private Long id;
     private String name;
     private Integer age;
-
     private List<OrderResponseDto> orders;
 
     public static MemberResponseDto of(Member member, List<Order> orders) {
